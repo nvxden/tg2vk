f8b55a282bc0b64b6204d65c6d9b4d689472b7b4
nvxden <clever411@yandex.ru>
2022-08-16 14:12:34 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,14 @@
+TG_TOKEN              = ''
+TG_UPDATE_DELAY       = 300 # milliseconds
+TG_UPDATE_COUNT       = 10  # times
+
+VK_USER_ID            = 0
+VK_TOKEN              = ''
+VK_APPLICATION_ID     = 0
+VK_CLIENT_SECRET      = ''
+VK_AUTH_REDIRECT_URI  = 'https://oauth.vk.com/blank.html'
+VK_AUTH_DISPLAY_PARAM = 'page'
+VK_AUTH_SCOPE         = 'wall,groups,photos,audio,video'
+VK_LOGIN              = ''
+VK_PASSWORD           = ''
+VK_GROUP_ID           = -0
